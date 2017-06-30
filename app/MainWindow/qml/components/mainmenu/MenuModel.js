a0f619641e75f0f71798261a6a353233ffe0e8b3
ikit <olivier@labsquare.org>
2017-06-30 11:39:53 +0200

    missing file

---
+++ app/MainWindow/qml/components/mainmenu/MenuModel.js
@@ -0,0 +1,14 @@
+ 
+var model = {
+    "menu" : [
+        { "icon": "a", "label": "Welcom",     "page" : "WelcomPage.qml", "sublevel": [], "collapse": false},
+        { "icon": "c", "label": "Project",    "page" : "ProjectPage.qml", "sublevel": [], "collapse": false},
+        { "icon": "b", "label": "Subject",    "page" : "SubjectPage.qml", "sublevel": [], "collapse": false},
+        { "icon": "d", "label": "Settings",   "page" : "SettingsPage.qml", "sublevel": [], "collapse": false},
+        { "icon": "e", "label": "Help",       "page" : "HelpPage.qml", "sublevel": [], "collapse": false},
+        { "icon": "f", "label": "About",      "page" : "AboutPage.qml", "sublevel": [], "collapse": false},
+        { "icon": "g", "label": "Disconnect", "page" : "DisconnectPage.qml", "sublevel": [], "collapse": false},
+        { "icon": "h", "label": "Close",      "page" : "ClosePage.qml", "sublevel": [], "collapse": false}
+
+    ]
+}
