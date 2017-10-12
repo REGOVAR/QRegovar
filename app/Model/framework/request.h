c08f43cfec365f545d461e01cd17c6d2ac300e4e
ikit <olivier@labsquare.org>
2017-10-12 12:34:52 +0200

    add missing files

---
+++ app/Model/framework/request.h
@@ -0,0 +1,55 @@
+#ifndef REQUEST_H
+#define REQUEST_H
+#include <QObject>
+#include <QtNetwork>
+#include <QJsonObject>
+#include <QJsonDocument>
+
+
+
+class Request : public QObject
+{
+    Q_OBJECT
+public:
+    enum Verb { Get, Post, Put, Del, Patch, Head};
+    Q_ENUM(Verb)
+    Request(Verb verb, const QString& query, QHttpMultiPart* data=nullptr, QObject* parent=0);
+    Request(Verb verb, const QString& query, const QByteArray& data, QObject* parent=0);
+    static QNetworkAccessManager* netManager();
+    // Factories
+    static Request* get_url(const QString& query) ;
+    static Request* get(const QString& query) ;
+    static Request* post(const QString& query, QHttpMultiPart* data=nullptr);
+    static Request* post(const QString& query, const QByteArray& data);
+    static Request* put(const QString& query,  QHttpMultiPart* data=nullptr);
+    static Request* put(const QString& query,  const QByteArray& data);
+    static Request* del(const QString& query);
+    // static Request* patch(const QString query,const QByteArray& data = QByteArray());
+    // static Request* head(const QString query);
+    static QNetworkRequest makeRequest(const QString& resource) ;
+
+
+    // Accessors
+    const QJsonObject& json() const;
+    inline bool success() { return mSuccess; }
+    inline bool loading() { return mLoading; }
+    inline QNetworkReply::NetworkError replyError() { return mReplyError; }
+
+
+Q_SIGNALS :
+    void responseReceived(bool success, const QJsonObject& json);
+
+protected Q_SLOTS:
+    void received();
+
+private:
+    static QNetworkAccessManager* mNetManager;
+
+    bool mLoading = false;
+    bool mSuccess = false;
+    QNetworkReply::NetworkError mReplyError;
+    QNetworkReply* mReply;
+    QJsonObject mJson;
+};
+
+#endif // REQUEST_H
---
+++ app/Model/framework/treeitem.h
@@ -0,0 +1,47 @@
+#ifndef TREEITEM_H
+#define TREEITEM_H
+
+#include <QList>
+#include <QHash>
+#include <QVariant>
+
+class TreeItem : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TreeItem(TreeItem* parent=nullptr);
+    explicit TreeItem(const QHash<int, QVariant> &data, TreeItem* parent=nullptr);
+    ~TreeItem();
+
+    Q_INVOKABLE void appendChild(TreeItem *child);
+
+    Q_INVOKABLE TreeItem *child(int number);
+    Q_INVOKABLE int childCount() const;
+    Q_INVOKABLE int columnCount() const;
+    Q_INVOKABLE QVariant data(int column) const;
+    Q_INVOKABLE int row() const;
+    Q_INVOKABLE TreeItem *parent();
+    Q_INVOKABLE void recursiveDelete();
+    Q_INVOKABLE void setData(const QHash<int, QVariant> &data);
+
+    // Lazy loading features
+    inline int virtualChildCount() { return mVirtualChildCount; }
+    inline void setVirtualChildCount(int count) { mVirtualChildCount = count; }
+
+//    int childNumber() const;
+//    bool insertChildren(int position, int count, int columns);
+//    bool insertColumns(int position, int columns);
+//    bool removeChildren(int position, int count);
+//    bool removeColumns(int position, int columns);
+//    bool setData(int column, const QVariant &value);
+
+protected:
+    QList<TreeItem*> mChildItems;
+    QHash<int, QVariant> mItemData;
+    TreeItem *mParentItem;
+    bool mIsSelected;
+
+    int mVirtualChildCount; // number of children (used to know if need to load children)
+};
+
+#endif // TREEITEM_H 
