93662c90685cb12eaf8bd2ac1b7ac36fa049bd6c
Olivier Gueudelot <olivier@labsquare.org>
2018-06-07 23:03:19 +0200

    Update build-msvc.bat

--- deploy/win/build-msvc.bat
+++ deploy/win/build-msvc.bat
@@ -16,26 +16,18 @@ if "%PLATFORM%" == "static" set VC_VARSALL=amd64
 set qtplatform=%PLATFORM%
 
 call %VC_DIR% %VC_VARSALL%
-echo "step 1"
 
 
 
 for %%* in (.) do set CurrDirName=%%~nx*
 
-echo "step 2"
-
 echo C:\Qt\%QT_VER%\%qtplatform%\bin\qmake
 ls C:\Qt\%QT_VER%\%qtplatform%\bin\
 C:\Qt\%QT_VER%\%qtplatform%\bin\qmake .\app\ || exit /B 1
-echo "step 3"
+
 
 nmake qmake_all || exit /B 1
-echo "step 4"
-
 nmake || exit /B 1
-echo "step 5"
-
-nmake lrelease || exit /B 1
-echo "step 6"
+nmake release || exit /B 1
 
 nmake INSTALL_ROOT=\projects\%CurrDirName%\install install || exit /B 1
