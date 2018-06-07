00cb08e2d378507c0f94f2ba479ada1f849e7b4d
Olivier Gueudelot <olivier@labsquare.org>
2018-06-07 21:41:23 +0200

    Update build-msvc.bat

--- deploy/win/build-msvc.bat
+++ deploy/win/build-msvc.bat
@@ -13,6 +13,8 @@ if "%PLATFORM%" == "msvc2015_64" set VC_VARSALL=amd64
 if "%PLATFORM%" == "msvc2015" set VC_VARSALL=amd64_x86
 if "%PLATFORM%" == "static" set VC_VARSALL=amd64
 
+set qtplatform=%PLATFORM%
+
 call %VC_DIR% %VC_VARSALL%
 echo "step 1"
 
@@ -22,9 +24,9 @@ for %%* in (.) do set CurrDirName=%%~nx*
 
 echo "step 2"
 
-echo C:\Qt\%QT_VER%\%PLATFORM%\bin\qmake
-ls C:\Qt\%QT_VER%\%PLATFORM%\bin\
-C:\Qt\%QT_VER%\%PLATFORM%\bin\qmake .\app\ || exit /B 1
+echo C:\Qt\%QT_VER%\%qtplatform%\bin\qmake
+ls C:\Qt\%QT_VER%\%qtplatform%\bin\
+C:\Qt\%QT_VER%\%qtplatform%\bin\qmake .\app\ || exit /B 1
 echo "step 3"
 
 nmake qmake_all || exit /B 1
