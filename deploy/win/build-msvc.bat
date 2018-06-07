d325340e3c3eaf01446815a7dacc9230d73fec72
Olivier Gueudelot <olivier@labsquare.org>
2018-06-07 13:34:35 +0200

    Update build-msvc.bat

--- deploy/win/build-msvc.bat
+++ deploy/win/build-msvc.bat
@@ -5,13 +5,10 @@ setlocal
 set qtplatform=%PLATFORM%
 for %%* in (.) do set CurrDirName=%%~nx*
 
-echo %VC_DIR% %VC_VARSALL%
-call %VC_DIR% %VC_VARSALL% || exit /B 1
-
 mkdir build-%qtplatform%
 cd build-%qtplatform%
 
-C:\projects\Qt\%QT_VER%\%qtplatform%\bin\qmake ../ || exit /B 1
+C:\Qt\%QT_VER%\%qtplatform%\bin\qmake ../ || exit /B 1
 nmake qmake_all || exit /B 1
 nmake || exit /B 1
 nmake lrelease || exit /B 1
