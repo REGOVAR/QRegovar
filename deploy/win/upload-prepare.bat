ae4fd2f46432c83188933c56398afecdf1d2c3e9
Olivier Gueudelot <olivier@labsquare.org>
2018-06-08 10:48:00 +0200

    Update upload-prepare.bat

--- deploy/win/upload-prepare.bat
+++ deploy/win/upload-prepare.bat
@@ -4,5 +4,11 @@ if "%PLATFORM%" == "static" (
 	7z a build_static_win_%QT_VER%.zip static_win || exit \B 1
 	move build_static_win_%QT_VER%.zip ..\..\..\ || exit \B 1
 ) else (
+	:: clean package
+	del C:\projects\qregovar\release\*.h
+	del C:\projects\qregovar\release\*.cpp
+	del C:\projects\qregovar\release\*.obj
+	ren "C:\projects\qregovar\release\QRegovar.exe" "C:\projects\qregovar\release\regovar.exe"
+
 	7z a build_%PLATFORM%_%QT_VER%.zip C:\projects\qregovar\release || exit \B 1
 )
